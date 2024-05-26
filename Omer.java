6c80184a515442d980a6e7c48bca86269ab5487f
omersh1998 <39498163+omersh1998@users.noreply.github.com>
2024-05-26 22:24:57 +0300

    Update Omer.java

--- Omer.java
+++ Omer.java
@@ -1,6 +1,6 @@
 public class Omer {
 	public static void main(String[] args) { 
-		for (int i = 1; i <= 5; i++) {
+		for (int i = 1; i <= 10; i++) {
 			System.out.println("Omer Sharoni " + i);
 		}
 	}
